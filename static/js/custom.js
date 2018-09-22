30268b0fbfab96a91520c50ea88b4990a0527203
Ryan Thompson <rtdilbert2@gmail.com>
2018-09-22 16:22:24 -0400

    Update custom.js
    simplified in text scripts all into custom.js file

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,33 @@
-$(document).ready(function () {
-    jQuery(document).ready(function($) {
-        $(".table-secondary").click(function() {
+$(document).ready(function () 
+{
+    jQuery(document).ready(function($) 
+	{
+        $(".table-secondary").click(function() 
+		{
             window.location = $(this).data("href");
         });
-    });
+		$(".card").click(function () 
+		{
+            window.location = $(this).data("href");
+        });
+		$(".table-info").click(function () 
+		{
+            window.location = $(this).data("href");
+		});
+        $(".table-warning").click(function () {
+            window.location = $(this).data("href");
+        });
+        $(".table-danger").click(function () 
+		{
+            window.location = $(this).data("href");
+        });
+        $(".table-success").click(function () 
+		{
+            window.location = $(this).data("href");
+        });
+        $(".alert-info").click(function () 
+		{
+            window.location = $(this).data("href");
+        });
+    });     
 });
